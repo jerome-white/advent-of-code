ca8a2a56f86950781f9a30a9ec267e18f7902b61
Jerome White <jerome@thewex.net>
2024-09-21 15:36:47 +0530

    Solution to problem 9 part 2

--- 2023/09/solution.py
+++ 2023/09/solution.py
@@ -1,32 +1,73 @@
 import sys
 import logging
+import operator as op
 import itertools as it
 from argparse import ArgumentParser
 from multiprocessing import Pool, Queue
 
-def sense(values):
-    if not any(values):
-        return 0
+class Sensor:
+    def __init__(self, combine, index):
+        self.combine = combine
+        self.index = index
 
-    iterable = zip(values, it.islice(values, 1, None))
-    values_ = [ y - x for (x, y) in iterable ]
+    def __call__(self, values):
+        if not any(values):
+            return 0
 
-    return values[-1] + sense(values_)
+        iterable = zip(values, it.islice(values, 1, None))
+        values_ = [ y - x for (x, y) in iterable ]
 
-def func(args):
-    logging.warning(args)
-    return sense(args)
+        return self.combine(values[self.index], self(values_))
 
-def scanf(fp):
-    for line in fp:
-        yield list(map(int, line.strip().split()))
+class RightSensor(Sensor):
+    def	__init__(self):
+        super().__init__(op.add, -1)
 
+class LeftSensor(Sensor):
+    def	__init__(self):
+        super().__init__(op.sub, 0)
+
+#
+#
+#
+def func(incoming, outgoing, version):
+    sensor = RightSensor() if version == 1 else LeftSensor()
+
+    while True:
+        values = incoming.get()
+
+        answer = sensor(values)
+        logging.warning(f'{values}: {answer}')
+
+        outgoing.put(answer)
+
+def extrapolate(args, fp):
+    incoming = Queue()
+    outgoing = Queue()
+    initargs = (
+        outgoing,
+        incoming,
+        args.version,
+    )
+
+    with Pool(args.workers, func, initargs):
+        jobs = 0
+        for line in fp:
+            values = list(map(int, line.strip().split()))
+            outgoing.put(values)
+            jobs += 1
+
+        for _ in range(jobs):
+            answer = incoming.get()
+            yield answer
+
+#
+#
+#
 if __name__ == '__main__':
     arguments = ArgumentParser()
     arguments.add_argument('--version', type=int, default=1, choices=(1, 2))
     arguments.add_argument('--workers', type=int)
     args = arguments.parse_args()
 
-    with Pool(args.workers) as pool:
-        values = pool.imap_unordered(func, scanf(sys.stdin))
-        print(sum(values))
+    print(sum(extrapolate(args, sys.stdin)))
