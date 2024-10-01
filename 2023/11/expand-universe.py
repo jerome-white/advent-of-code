0563b566d31aac5e052201e7602d36bc03e008d1
Jerome White <jerome@thewex.net>
2024-10-01 16:00:08 +0530

    Bug fix and refactor
    * Column insertion was making new rows with width equal to the number
      of rows. The calculation should have been based on columns.
    
    * Use template to perform row and colum insertion

--- 2023/11/expand-universe.py
+++ 2023/11/expand-universe.py
@@ -1,36 +1,83 @@
 import sys
+import logging
+import itertools as it
 
+#
+#
+#
 class Universe:
-    _empty = '.'
+    @property
+    def nrow(self):
+        return len(self.sky)
+
+    @property
+    def ncol(self):
+        return max(map(len, self.sky))
 
     def __init__(self, fp):
         self.sky = [ list(x.strip()) for x in fp ]
-        (self.nrow, self.ncol) = (len(self.sky), len(self.sky[0]))
 
     def __str__(self):
         return '\n'.join(''.join(x) for x in self.sky)
 
-    def rows(self):
-        r = 0
-        while r < self.nrow:
-            row = self.sky[r]
-            if all(x == self._empty for x in row):
-                row_ = [ self._empty ] * self.nrow
-                self.sky.insert(r, row_)
-                r += 2
-            else:
-                r += 1
-
-    def cols(self):
-        for i in range(self.ncol - 1, -1, -1):
-            if all(self.sky[x][i] == self._empty for x in range(self.nrow)):
-                for r in self.sky:
-                    r.insert(i, '.')
-
     def expand(self):
-        self.rows()
-        self.cols()
+        for E in (RowExpander, ColumnExpander):
+            expander = E(self)
+            expander()
 
+#
+#
+#
+class UniverseExpander:
+    _empty = '.'
+
+    def __init__(self, universe):
+        self.universe = universe
+
+    def __call__(self):
+        i = 0
+        while self.proceed(i):
+            if all(self.empty(i)):
+                self.insert(i)
+                i += 2
+            else:
+                i += 1
+
+    def proceed(self, loc):
+        raise NotImplementedError()
+
+    def empty(self, loc):
+        raise NotImplementedError()
+
+    def insert(self, loc):
+        raise NotImplementedError()
+
+class RowExpander(UniverseExpander):
+    def proceed(self, loc):
+        return loc < self.universe.nrow
+
+    def empty(self, loc):
+        yield from (x == self._empty for x in self.universe.sky[loc])
+
+    def insert(self, loc):
+        row = [ self._empty ] * self.universe.ncol
+        self.universe.sky.insert(loc, row)
+
+class ColumnExpander(UniverseExpander):
+    def proceed(self, loc):
+        return loc < self.universe.ncol
+
+    def empty(self, loc):
+        iterable = range(self.universe.nrow)
+        yield from (self.universe.sky[x][loc] == self._empty for x in iterable)
+
+    def insert(self, loc):
+        for row in self.universe.sky:
+            row.insert(loc, self._empty)
+
+#
+#
+#
 if __name__ == '__main__':
     universe = Universe(sys.stdin)
     universe.expand()
