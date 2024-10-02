f42a7c9f335b94dd29739acdbf31ddff75a8a529
Jerome White <jerome@thewex.net>
2024-10-02 12:00:17 +0530

    Smarter solution to problem 11
    Previous solution was brute force, which took too long for part 2 of
    the problem. Instead of expanding the input, this version adjusts the
    distance based on the empty space between the galaxies.

--- 2023/11/solution.py
+++ 2023/11/solution.py
@@ -4,18 +4,34 @@ import functools as ft
 import itertools as it
 import collections as cl
 from argparse import ArgumentParser
-from multiprocessing import Pool, Queue
+from multiprocessing import Pool
 
-import networkx as nx
+from scipy.spatial.distance import cityblock
 
 Coordinate = cl.namedtuple('Coordinate', 'row, col')
 
-class Grid:
+class Universe:
+    _empty = '.'
     _galaxy = '#'
 
     @ft.cached_property
-    def shape(self):
-        return (len(self.grid), len(self.grid[0]))
+    def dark_r(self):
+        def dark():
+            for (r, row) in enumerate(self.grid):
+                if all(x == self._empty for x in row):
+                    yield r
+
+        return tuple(dark())
+
+    @ft.cached_property
+    def dark_c(self):
+        def dark():
+            (nrow, ncol) = (len(self.grid), len(self.grid[0]))
+            for c in range(ncol):
+                if all(self.grid[x][c] == self._empty for x in range(nrow)):
+                    yield c
+
+        return tuple(dark())
 
     def __init__(self, fp):
         self.grid = [ x.strip() for x in fp ]
@@ -26,39 +42,43 @@ class Grid:
                 if cell == self._galaxy:
                     yield Coordinate(r, c)
 
-def func(incoming, outgoing, grid):
-    G = nx.grid_2d_graph(*grid.shape)
+#
+#
+#
+def func(args):
+    (u, v, dark, expansion) = args
 
-    while True:
-        (u, v) = incoming.get()
-        plength = nx.shortest_path_length(G, source=u, target=v)
-        logging.warning('%s -> %s: %d', u, v, plength)
-        outgoing.put(plength)
+    distance = cityblock(u, v)
+    for (d, *e) in zip(dark, u, v):
+        (l, r) = sorted(e)
+        distance += sum(l < x < r for x in d) * expansion
+    logging.warning('%s %s: %d', u, v, distance)
 
-def shortest_paths(args, fp):
-    grid = Grid(fp)
+    return distance
 
-    incoming = Queue()
-    outgoing = Queue()
-    initargs = (
-        outgoing,
-        incoming,
-        grid,
+def each(args, fp):
+    if args.expansion is None:
+        expansion = 2 if args.version == 1 else int(1e6)
+    else:
+        expansion = args.expansion
+    expansion -= 1
+
+    universe = Universe(fp)
+    dark = (
+        universe.dark_r,
+        universe.dark_c,
     )
 
-    with Pool(args.workers, func, initargs):
-        jobs = 0
-        for e in it.combinations(grid, r=2):
-            outgoing.put(e)
-            jobs += 1
-
-        for _ in range(jobs):
-            result = incoming.get()
-            yield result
+    for e in it.combinations(universe, r=2):
+        yield (*e, dark, expansion)
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
+    arguments.add_argument('--expansion', type=int)
+    arguments.add_argument('--version', type=int, default=1, choices=(1, 2))
     arguments.add_argument('--workers', type=int)
     args = arguments.parse_args()
 
-    print(sum(shortest_paths(args, sys.stdin)))
+    with Pool(args.workers) as pool:
+        distances = pool.imap_unordered(func, each(args, sys.stdin))
+        print(sum(distances))
