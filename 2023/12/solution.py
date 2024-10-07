7e2bd38e52c11eae3c8144d450bbdf921f1d657f
Jerome White <jerome@thewex.net>
2024-10-07 18:06:40 +0530

    Solves a tangential version of part 1
    This version solves the problem, but a more generic form of it. After
    running this it appears the order of the layout is important; that is
    not taken into account in this version. Checking this in because it
    has useful logic on which to iterate.

--- 2023/12/solution.py
+++ 2023/12/solution.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import logging
 import collections as cl
 from dataclasses import dataclass
 
@@ -8,25 +9,14 @@ class ConditionRecord:
     springs: str
     layout: tuple
 
-    def __post_init__(self):
-        self._counts = cl.Counter(self.layout)
-
-    def __bool__(self):
-        counts = dict(self._counts)
-        for dots in self.springs.split('.'):
-            if not all(x == '#' in x in dots):
-                return False
-            counts[len(dots)] -= 1
-
-        return not any(counts.values())
-
     def simplify(self):
         (temp, pound) = ('-', '#')
         assert temp not in self.springs
         layout = []
+        counts = cl.Counter(self.layout)
 
         springs = self.springs
-        for (k, v) in self._counts.items():
+        for (k, v) in counts.items():
             # gpt-4o prompt:
             #  Write a regular expression using Python's re library
             #  that does the following:
@@ -45,20 +35,52 @@ class ConditionRecord:
                     middle = (springs[left:right]
                               .replace(pound, temp)
                               .replace('?', '.'))
-                    springs = f'{springs[:left]}{middle}{springs[right:]}'
+                    springs = substitute(springs, middle, left, right)
             else:
                 layout.extend(k for _ in range(v))
         springs = springs.replace(temp, pound)
 
         return type(self)(springs, tuple(layout))
 
+class ArrangementIterator:
+    def __init__(self, record):
+        self.record = record
+        self.counts = cl.Counter(self.record.layout)
+
+    def __str__(self):
+        return str(self.record)
+
+    def __iter__(self):
+        record = self.record.simplify()
+        yield from self(record.springs, record.layout)
+
+    def __call__(self, springs, layout):
+        if not layout:
+            s = springs.replace('?', '.')
+            if self.accept(s):
+                yield s
+        else:
+            (head, *tail) = layout
+            pounds = '#' * head
+            for m in re.finditer(f'[^.]{{{head}}}', springs):
+                s = substitute(springs, pounds, *m.span())
+                yield from self(s, tail)
+
+    def accept(self, springs):
+        counts = cl.Counter(map(len, filter(None, springs.split('.'))))
+        return counts == self.counts
+
+def substitute(word, replacement, left, right):
+    return f'{word[:left]}{replacement}{word[right:]}'
+
 def records(fp):
     for line in fp:
         (springs, layout) = line.strip().split()
         layout = tuple(map(int, layout.split(',')))
-
         yield ConditionRecord(springs, layout)
 
 if __name__ == '__main__':
     for r in records(sys.stdin):
-        print(r, r.simplify(), '', sep='\n')
+        aitr = ArrangementIterator(r)
+        arrangements = set(aitr)
+        logging.critical(f'{r} {len(arrangements)}')
