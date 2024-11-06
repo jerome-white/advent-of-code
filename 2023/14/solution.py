664e1285637684ac452805aa8db2cfb831dc4ea3
Jerome White <jerome@thewex.net>
2024-11-06 15:01:09 +0000

    Compartmentalize panel tracing into classes

--- 2023/14/solution.py
+++ 2023/14/solution.py
@@ -1,8 +1,20 @@
 import sys
-import csv
 import logging
+import functools as ft
 import collections as cl
+from dataclasses import dataclass
 
+#
+#
+#
+@dataclass
+class PanelInstance:
+    rotation: int
+    panel: str
+
+#
+#
+#
 class PanelTracker:
     def __init__(self):
         self.panels = cl.OrderedDict()
@@ -26,40 +38,76 @@ class PanelTracker:
 #
 #
 #
+class PanelHandler:
+    def __init__(self, tracker, rotations):
+        self.tracker = tracker
+        self.rotations = rotations
+
+    def __str__(self):
+        raise NotImplementedError()
+
+class StandardPanel(PanelHandler):
+    def __str__(self):
+        trace = list(self.tracker.trace())
+        return trace[-1]
+
+class CyclicPanel(PanelHandler):
+    def __str__(self):
+        (trace, cycle) = map(list, (self.tracker.trace(), self.tracker))
+        (m, n) = map(len, (trace, cycle))
+        c = m - n
+        index = (self.rotations - c) % n
+
+        return cycle[index]
+
+#
+#
+#
+@ft.singledispatch
 def load(panel):
+    raise TypeError(type(panel))
+
+@load.register
+def _(panel: str):
     for (i, p) in enumerate(reversed(panel.splitlines()), 1):
         yield i * p.count('O')
+
+@load.register
+def _(panel: PanelHandler):
+    return load(str(panel))
 
 def scanf(fp):
     for line in fp:
         (index, panel) = line.rstrip().split()
-        yield (
-            int(index),
-            panel.replace(',', '\n'),
-        )
+        yield PanelInstance(int(index), panel.replace(',', '\n'))
 
-if __name__ == '__main__':
-    panels = PanelTracker()
+def handle(panels):
+    tracker = PanelTracker()
     rotations = None
-    for (i, row) in scanf(sys.stdin):
-        if rotations is None:
-            rotations = i
-        try:
-            panels.push(row)
-        except ValueError:
-            break
-        logging.info('{} {} {}'.format(
-            i,
-            row.replace('\n', '|'),
-            sum(load(row)),
-        ))
 
-    (t_full, t_cycle) = map(list, (panels.trace(), panels))
-    if t_cycle:
-        (m, n) = map(len, (t_full, t_cycle))
-        c = m - n
-        index = (rotations - c) % n
-        panel = t_cycle[index]
+    for p in panels:
+        if rotations is None:
+            rotations = p.rotation
+
+        try:
+            tracker.push(p.panel)
+        except ValueError:
+            MyPanelHandler = CyclicPanel
+            break
+
+        logging.info(
+            '%d %s %d',
+            p.rotation,
+            p.panel.replace('\n', '|'),
+            sum(load(p.panel)),
+        )
     else:
-        panel = t_full[-1]
-    print(sum(load(panel)))
+        MyPanelHandler = StandardPanel
+
+    return MyPanelHandler(tracker, rotations)
+
+#
+#
+#
+if __name__ == '__main__':
+    print(sum(load(handle(scanf(sys.stdin)))))
