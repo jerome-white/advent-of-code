dfb41d25edd74c41923cedcd258ed63777ed9f8e
Jerome White <jerome@thewex.net>
2024-11-15 18:43:40 +0530

    More attempts
    * Corrected history tracking
    * Improved path output

--- 2023/17/solution.py
+++ 2023/17/solution.py
@@ -21,34 +21,71 @@ class Coordinate:
         return type(self)(self.row + other.row, self.col + other.col)
 
 class History:
-    def __init__(self, maxlen=None, past=None):
-        self.history = past or cl.deque(maxlen=maxlen)
+    # https://docs.python.org/3/library/itertools.html#itertools-recipes
+    @staticmethod
+    def all_equal(iterable):
+        g = it.groupby(iterable)
+        return next(g, True) and not next(g, False)
 
-    def __iter__(self):
-        yield from zip(self.history, it.islice(self.history, 1, None))
+    def __init__(self, maxlen=None, history=None):
+        assert bool(maxlen) ^ bool(history)
+        self.history = history or cl.deque(maxlen=maxlen)
 
     def __bool__(self):
-        return len(self.history) < self.history.maxlen or not self.all_equal()
+        n = len(self.history)
+        if n < self.history.maxlen:
+            return True
 
-    # https://docs.python.org/3/library/itertools.html#itertools-recipes
-    def all_equal(self):
-        g = it.groupby(self.history)
-        return next(g, True) and not next(g, False)
+        n -= self.history.maxlen
+        return not self.all_equal(it.islice(self.history, n, None))
 
     def __add__(self, other):
         history = self.history.copy()
         history.append(other)
 
-        return type(self)(past=history)
+        return type(self)(history=history)
+
+    def peek(self):
+        return self.history[-1] if self.history else None
+
+class BoardNavigator:
+    _navigation = {
+        '^': (-1, 0), # up
+        '>': ( 0, 1), # right
+        'v': ( 1, 0), # down
+    }
+
+    def __init__(self, missing='*'):
+        self.missing = missing
+        self.navigation = {
+            Coordinate(*y): x for (x, y) in self._navigation.items()
+        }
+
+    def __iter__(self):
+        yield from self.navigation
+
+    def to_string(self, coord):
+        if coord not in self.navigation:
+            return self.missing
+
+        return self.navigation[coord]
 
 class Board:
     @ft.cached_property
     def shape(self):
         return tuple(len(x) for x in (self.board, self.board[0]))
 
+    @ft.cached_property
+    def start(self):
+        return self.board[0][0]
+
     def __init__(self, board):
         self.board = board
+        self.navigation = BoardNavigator()
         self.end = Coordinate(*(x - 1 for x in self.shape))
+
+    def __iter__(self):
+        yield from self.navigation
 
     def __getitem__(self, key):
         return self.board[key.row][key.col]
@@ -61,33 +98,29 @@ class Board:
 
     def to_strings(self, special=None):
         if special is None:
-            special = set()
+            special = {}
 
         for (r, row) in enumerate(self.board):
             record = []
             for (c, cell) in enumerate(row):
                 coord = Coordinate(r, c)
-                rec = ' ' if coord in special else str(cell)
+                if coord in special:
+                    rec = self.navigation.to_string(special.get(coord))
+                else:
+                    rec = str(cell)
                 record.append(rec)
             yield ''.join(record)
 
 class MachinePartsFactory:
-    _navigation = (
-        (-1, 0), # up
-        ( 0, 1), # right
-        ( 1, 0), # down
-    )
-
     def __init__(self, board, history):
         self.board = board
         self.history = history
 
-        self.navigation = tuple(it.starmap(Coordinate, self._navigation))
         self.lower = None
-        self.visited = set()
+        self.visited = {}
 
     def __int__(self):
-        return self.lower
+        return self.lower - self.board.start
 
     def __call__(self):
         self.visited.clear()
@@ -101,17 +134,17 @@ class MachinePartsFactory:
         if self.lower is not None and heat >= self.lower:
             return
 
+        self.visited[coord] = path.peek()
         if self.board.target(coord):
             self.lower = heat
             if logging.getLogger().isEnabledFor(logging.WARNING):
                 for s in self.board.to_strings(self.visited):
                     logging.warning(s)
-            logging.critical(self.lower)
+            logging.critical(int(self))
         else:
-            self.visited.add(coord)
-            for c in self.navigation:
+            for c in self.board:
                 self.walk(coord + c, path + c, heat)
-            self.visited.discard(coord)
+        self.visited.pop(coord)
 
 def scanf(fp):
     for row in fp:
@@ -128,7 +161,7 @@ if __name__ == '__main__':
     args = arguments.parse_args()
 
     board = Board(list(scanf(sys.stdin)))
-    history = History(args.max_direction)
+    history = History(args.max_direction + 1)
 
     machine = MachinePartsFactory(board, history)
     machine()
